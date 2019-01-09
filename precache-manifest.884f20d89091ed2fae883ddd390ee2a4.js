582372739d80ab60aced21534c961ee3fb8c4f5a
Aakriti <aakriti1711@gmail.com>
2019-01-09 14:55:00 -0800

    Updates

---
+++ precache-manifest.884f20d89091ed2fae883ddd390ee2a4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "f9a6aa4118eb6c95d5e9",
+    "url": "/react-tutorial/static/js/main.f9a6aa41.chunk.js"
+  },
+  {
+    "revision": "855276873adeae1ea0f1",
+    "url": "/react-tutorial/static/js/1.85527687.chunk.js"
+  },
+  {
+    "revision": "f9a6aa4118eb6c95d5e9",
+    "url": "/react-tutorial/static/css/main.dd73cf09.chunk.css"
+  },
+  {
+    "revision": "93e6c7b5bb59215554c6acfdb4f02a5c",
+    "url": "/react-tutorial/index.html"
+  }
+];
