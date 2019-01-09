513010a8de3def2c32975706616fe3042b70ec5f
Aakriti <aakriti1711@gmail.com>
2019-01-09 12:52:03 -0800

    test commit

---
+++ src/Apps.js
@@ -0,0 +1,34 @@
+import React, { Component } from "react";
+import Table from "./Table";
+import Form from "./Form";
+
+class Apps extends Component {
+  state = {
+    userDetail: [
+    ]
+  };
+  deleteUserData = index => {
+    const { userDetail } = this.state;
+    this.setState({
+      userDetail: userDetail.filter((user, i) => {
+        return i !== index;
+      })
+    });
+  };
+  handleSubmit = userDetail => {
+    this.setState({ userDetail: [...this.state.userDetail, userDetail] });
+  };
+
+  render() {
+    const { userDetail } = this.state;
+
+    return (
+      <div className="container">
+        <Table userDetail={userDetail} deleteUserData={this.deleteUserData} />
+        <Form handleSubmit={this.handleSubmit} />
+      </div>
+    );
+  }
+}
+
+export default Apps;
